243813971570972700bf535335a131c1bf7665e5
Galuh Danutirto <galuh.danutirto@opencloud.id>
2023-12-06 02:07:51 +0700

    Initial Commit

---
+++ src/main/java/sda/catalogue/sdacataloguerestapi/SdaCatalogueRestApiApplication.java
@@ -0,0 +1,13 @@
+package sda.catalogue.sdacataloguerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaCatalogueRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaCatalogueRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sda/catalogue/sdacataloguerestapi/SdaCatalogueRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sda.catalogue.sdacataloguerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaCatalogueRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
