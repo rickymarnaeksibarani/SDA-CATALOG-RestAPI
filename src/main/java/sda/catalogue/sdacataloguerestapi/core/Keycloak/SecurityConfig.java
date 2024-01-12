31d23fa460161fbfba90b24c4693db827af77238
Galuh Danutirto <galuh.danutirto@opencloud.id>
2024-01-12 14:13:16 +0700

    [update] clean clode

--- src/main/java/sda/catalogue/sdacataloguerestapi/core/Keycloak/SecurityConfig.java
+++ src/main/java/sda/catalogue/sdacataloguerestapi/core/Keycloak/SecurityConfig.java
@@ -1,19 +1,19 @@
-package sda.catalogue.sdacataloguerestapi.core.Keycloak;
+//package sda.catalogue.sdacataloguerestapi.core.Keycloak;
+//
+//import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
-        httpSecurity.csrf().disable().authorizeHttpRequests().anyRequest().authenticated();
-        httpSecurity.oauth2ResourceServer().jwt();
-        httpSecurity.sessionManagement().se
-    }
-}
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+//        httpSecurity.csrf().disable().authorizeHttpRequests().anyRequest().authenticated();
+//        httpSecurity.oauth2ResourceServer().jwt();
+//        httpSecurity.sessionManagement().se
+//    }
+//}
