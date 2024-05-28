de28d2734afd7bde117b00f092ff8571eed2f4d5
Rohmat Tri Yulianto <rohmat.tri@gmf-aeroasia.co.id>
2024-05-28 09:43:55 +0700

    build.sh edited online with Bitbucket

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-mvn clean package -DskipTests && docker build -f alpine.Dockerfile -t microservice/sda-catalogue:latest . && docker-compose up -d
+mvn clean package -DskipTests && docker build -f alpine.Dockerfile -t microservice/sda-catalogue:latest . && docker-compose up -d --force-recreate
