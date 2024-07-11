7be835fc05707b50f5fb1d43b06cbf7dd183a4f4
Ricky Marnaek Sibarani <ricky1931072@itpln.ac.id>
2024-07-11 16:19:35 +0700

    clone from my project office

--- build.sh
+++
@@ -1,1 +0,0 @@
-mvn clean package -DskipTests && docker build -f alpine.Dockerfile -t microservice/sda-catalogue:latest . && docker-compose up -d --force-recreate && docker stop sda-catalog-rest-api && docker rm sda-catalog-rest-api
